ea5a2bdbbe3517288c691e982ce829f83240a767
PedroAsus <dossantp@lafayette.edu>
2026-02-27 13:16:05 -0500

    Improve install script + uncommit .venv

--- init.sh
+++ init.sh
@@ -11,82 +11,122 @@ SECRETS_DIR=$ROOT_DIR/"secrets"
 POSTGRES_ENV="postgres.env"
 DOCKER_ENV="docker.env"
 
-# Init
-mkdir -p $SECRETS_DIR
+DEFAULT_PG_PASSWORD="postgres"
+DEFAULT_PG_DATABASE="spellbookdb"
+DEFAULT_PG_DATA="/var/lib/postgresql/18/docker"
+DEFAULT_TIMEZONE="America/New_York"
 
-# Setting up python
-read -rp $'Setup Python virtual environment? [Y/n] ' -n 1
-echo ""
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-    echo "Setting up python virtual environment..."
+main() {
+    if ((BASH_VERSINFO[0] < 4))
+    then
+        echo "Please update to at least bash-4.0 to run this script."
+        exit 1
+    fi
+
+    prompt "Clean install?"
+    if [[ $RESULT ]]; then
+        echo ""
+        git clean -dfX
+        echo ""
+    fi
+
+    init
+    setup_tool PYTHON
+    setup_tool POSTGRES
+    setup_tool DOCKER
+    setup_tool TRADING_BOT
+}
+
+init() {
+    mkdir -p $SECRETS_DIR
+}
+
+# Usage: add_env <file> <key> <value>
+add_env() {
+    local file=$1
+    local key=$2
+    local value=$3
+
+    touch "$file"
+
+    # || true supresses the error code in cases where the file is empty
+    # Otherwise, the bash script would prematurely terminate
+    grep --invert-match "$key" "$file" > "temp.env" || true
+    echo "$key=$value" >> "temp.env"
+    mv "temp.env" "$file"
+}
+
+# Usage: prompt <prompt_string>
+# Return: 1: True, 0: False
+prompt() {
+    local prompt_string=$1
+
+    prompt_string="$prompt_string [Y/n] "
+
+    read -rp "$prompt_string" -n 1
+    if [[ ! $REPLY =~ ^[Nn]$ ]]; then
+        RESULT=1
+    else
+        RESULT=0
+    fi
+}
+
+# Usage setup_tool <tool>
+setup_tool() {
+    local tool=$1
+
+    prompt "Setup $tool?"
+    if [[ $RESULT ]]; then
+        echo ""
+        echo "Setting up $tool..."
+        $tool
+    fi
+
+    cd $ROOT
+    echo "Done!"
+    echo ""
+}
+
+# Sets up python venv
+PYTHON() {
     cd $MARKET_MATCHING_DIR
 
     python3 -m venv .venv
     source .venv/bin/activate
-    python -m pip install --upgrade pip
+    python3 -m pip install --upgrade pip
     pip install -r requirements.txt
     deactivate
+}
 
-    cd $ROOT
-    echo "Done!"
-    echo ""
-fi
+# Sets up
+POSTGRES() {
+    cd "$SECRETS_DIR"
 
-# Setting up Postgres
-read -rp $'Setup Postgres? [Y/n] ' -n 1
-echo ""
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-    echo "Setting up Postgres..."
+    read -rep $'Postgres Password:\n > ' -i $DEFAULT_PG_PASSWORD postgres_password
+    read -rep $'Postgres Database:\n > ' -i $DEFAULT_PG_DATABASE postgres_database
+    read -rep $'Postgres Data Path:\n > ' -i $DEFAULT_PG_DATA postgres_data
+
+    add_env "$POSTGRES_ENV" "POSTGRES_PASSWORD" "$postgres_password"
+    add_env "$POSTGRES_ENV" "POSTGRES_DB" "$postgres_database"
+    add_env "$POSTGRES_ENV" "PGDATA" "$postgres_data"
+    add_env "$DOCKER_ENV" "PGDATA" "$postgres_data"
+}
+
+DOCKER() {
     cd $SECRETS_DIR
+    read -rep $'Timezone:\n > ' -i "$DEFAULT_TIMEZONE" timezone
+    add_env "$DOCKER_ENV" "TIMEZONE" "$timezone"
 
-    read -rep $'Postgres Password:\n > ' -i "password123" postgres_password
-    read -rep $'Postgres Database:\n > ' -i "spellbookdb" postgres_database
-
-    rm $POSTGRES_ENV 2>/dev/null || true
-    echo "POSTGRES_PASSWORD="$postgres_password >> $POSTGRES_ENV
-    echo "POSTGRES_DB="$postgres_database >> $POSTGRES_ENV
-
-    cd $ROOT
-    echo "Done!"
-    echo ""
-fi
-
-# Setting up Docker
-read -rp $'Setup Docker? [Y/n] ' -n 1
-echo ""
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-    echo "Setting up Docker..."
-    cd $SECRETS_DIR
-
-    read -rep $'Timezone:\n > ' -i "America/New_York" timezone
-
-    rm $DOCKER_ENV 2>/dev/null || true
-    echo "TIMEZONE="$timezone >> $DOCKER_ENV
-
-    cd $DOCKER_DIR
+    cd "$DOCKER_DIR"
     sudo docker compose pull
+}
 
-    cd $ROOT
-    echo "Done!"
-    echo ""
-fi
-
-# Setting up trading bot + dashboard
-read -rp $'Setup Trading Bot and Dashboard? [Y/n] ' -n 1
-echo ""
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-    echo "Setting up Trading Bot + Dashboard"
-    cd $TRADING_BOT_DIR"/client"
+TRADING_BOT() {
+    cd "$TRADING_BOT_DIR/client"
     npm install
 
-    cd $TRADING_BOT_DIR"/server"
+    cd "$TRADING_BOT_DIR/server"
     npm install
+}
 
-    cd $ROOT
-    echo "Done!"
-    echo ""
-fi
+main
