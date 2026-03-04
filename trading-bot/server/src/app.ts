f038bbac9b8c9ad1063aa0c383b8df46e5fefb00
PedroAsus <dossantp@lafayette.edu>
2026-03-04 14:35:27 -0500

    Add cors

--- trading-bot/server/src/app.ts
+++ trading-bot/server/src/app.ts
@@ -1,6 +1,7 @@
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { createServer } from "http"
 import express from "express"
+import cors from "cors"
 
 import * as examples from "./examples"
 import * as log from "./util/log"
@@ -12,6 +13,7 @@ export const PORT = 3000
 const app = express()
 const server = createServer(app)
 
+app.use(cors())
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
