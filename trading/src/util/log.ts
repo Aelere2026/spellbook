bb600ae4573132b96feb68364804e4b540bcc02c
PedroAsus <dossantp@lafayette.edu>
2026-04-14 09:53:47 -0400

    Add customizable timestamp to logger

--- trading/src/util/log.ts
+++ trading/src/util/log.ts
@@ -1,11 +1,13 @@
 import c from "ansi-colors"
+import { NumberInstance } from "twilio/lib/rest/pricing/v2/number"
 import util from "util"
 
-type Verbosity = "DEBUG" | "LOG" | "INFO" | "ALERT" | "WARN" | "ERROR" | "FATAL"
 const verbosityOrder: Verbosity[] = ["DEBUG", "LOG", "INFO", "ALERT", "WARN", "ERROR", "FATAL"]
-
+type Verbosity = "DEBUG" | "LOG" | "INFO" | "ALERT" | "WARN" | "ERROR" | "FATAL"
 let loggerVerbosity: Verbosity = "DEBUG"
 
+type TimestampFormat = "NONE" | "TIME" | "DATETIME" | "UTC" | "ISO"
+let loggerTimestampFormat: TimestampFormat = "TIME"
 /**
  * Controls how verbose the logger should be.
  *
@@ -20,6 +22,10 @@ let loggerVerbosity: Verbosity = "DEBUG"
  */
 export function setVerbosity(verbosity: Verbosity) {
     loggerVerbosity = verbosity
+}
+
+export function setTimestampFormat(format: TimestampFormat) {
+    loggerTimestampFormat = format
 }
 
 export function debug(...msgs: any[]) {
@@ -54,9 +60,10 @@ export function newline() {
     console.log()
 }
 
-function print(msgVerbosity: Verbosity, format: c.StyleFunction, msgs: any[]) {
-    // TODO: indexOf probs isn't super performant? But also its 6 elements long so its probs okay?
-    if (verbosityOrder.indexOf(loggerVerbosity) <= verbosityOrder.indexOf(msgVerbosity)) {
+function print(verbosity: Verbosity, format: c.StyleFunction, msgs: any[]) {
+    // TODO: indexOf probs isn't super performant? But also its 7 elements long so its probs okay?
+    if (verbosityOrder.indexOf(loggerVerbosity) <= verbosityOrder.indexOf(verbosity)) {
+        const verbosityStr = c.bold(`[${verbosity}]`)
 
         // We use util.inspect so that our objects look normal and not like "[object Object]" when printing
         const msgsStr = msgs.map(msg => {
@@ -67,6 +74,48 @@ function print(msgVerbosity: Verbosity, format: c.StyleFunction, msgs: any[]) {
             }
         }).join(" ")
 
-        console.log(format(`${c.bold(`[${msgVerbosity}]`)} ${msgsStr}`))
+        const now = Date.now()
+        let timestampStr = ""
+        switch (loggerTimestampFormat) {
+            case "TIME":
+                timestampStr = time.format(now)
+                break
+            case "DATETIME":
+                timestampStr = dateTime.format(now)
+                break
+            case "UTC":
+                timestampStr = new Date(now).toUTCString()
+                break
+            case "ISO":
+                timestampStr = new Date(now).toISOString()
+                break
+            default: // FALLTHROUGH!!
+                console.error("Invalid logger format!!")
+            case "NONE":
+                timestampStr = ""
+                break
+        }
+        if (timestampStr !== "") {
+            timestampStr = c.bold(`[${timestampStr}] - `)
+        }
+
+        console.log(format(`${verbosityStr} ${timestampStr}${msgsStr}`))
     }
 }
+
+const time = new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false
+})
+
+const dateTime = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false
+})
