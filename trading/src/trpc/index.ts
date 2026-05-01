056506b32443e3a6fe79600c541bb169218f2427
sophieeckert <eckerts@lafayette.edu>
2026-05-01 08:53:10 -0400

    Register configRouter in tRPC app router

--- trading/src/trpc/index.ts
+++ trading/src/trpc/index.ts
@@ -3,14 +3,14 @@ import marketRouter from "./marketRouter"
 import platformRouter from "./platformRouter"
 import arbitrageRouter from "./arbitrageRouter"
 import matchRouter from "./matchRouter"
-
-
-
+import configRouter from "./configRouter"
+ 
 export const appRouter = router({
     markets: marketRouter,
-    platforms: platformRouter, 
+    platforms: platformRouter,
     arbitrages: arbitrageRouter,
     matches: matchRouter,
+    config: configRouter,
 })
-
+ 
 export type AppRouter = typeof appRouter
